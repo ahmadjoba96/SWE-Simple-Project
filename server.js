67343ea807d809ccfd37abddc041c5aa81a6d1d9
Ahmad Joba <ahmadjoba2005@gmail.com>
2025-12-07 18:26:43 +0200

    init project

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+// Middleware to parse JSON body
+app.use(express.json());
+
+// Simple test route
+app.get("/", (req, res) => {
+  res.send("API is running ");
+});
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
