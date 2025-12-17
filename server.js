e80483890bf3c39268d7f23099f0cfbf9095660d
Ahmad Joba <ahmadjoba2005@gmail.com>
2025-12-17 15:43:33 +0200

    edit server.js

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
-const connectDB = require("./src/config/db");
-const app = require("./app");
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Connect DB
+const connectDB = require("./src/config/db");
 connectDB();
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("API is running");
+});
+
+app.use("/api/auth", require("./src/routes/auth.routes"));
+app.use("/api/categories", require("./src/routes/category.routes"));
+app.use("/api/products", require("./src/routes/product.routes"));
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
