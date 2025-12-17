37ebc1f44571f2cbaa10a9eb3707fe6e7c19dd77
Ahmad Joba <ahmadjoba2005@gmail.com>
2025-12-17 15:57:04 +0200

    test Railway

--- server.js
+++ server.js
@@ -24,8 +24,12 @@ app.use("/api/products", require("./src/routes/product.routes"));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
 
 module.exports = app;
